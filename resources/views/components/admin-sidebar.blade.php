6b7077d0e2acfecb2450d401b5da98ada4b918fb
shakyamtech <shakya.mahes@gmail.com>
2025-02-07 23:15:44 +0545

    after error 1

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,31 +1,32 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="{{route('dashboard')}}"> <img alt="image" src="/assets/img/codeitlogo.webp" class="header-logo" /> <span
-          class="logo-name"></span>
-      </a>
+        <a href="{{ route('dashboard') }}"> <img alt="image" src="/assets/img/codeitlogo.webp" class="header-logo" />
+            <span class="logo-name"></span>
+        </a>
     </div>
     <ul class="sidebar-menu">
-      <li class="menu-header">Main</li>
-      <li class="dropdown active">
-        <a href="{{route('dashboard')}}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
-      </li>
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+            <a href="{{ route('dashboard') }}" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
 
-      <li class="dropdown">
-        <a href="{{route('admin.company.create')}}" class="nav-link"><i class="fas fa-building"></i><span>Company</span></a>
-      </li>
+        <li class="dropdown">
+            <a href="{{ route('admin.company.index') }}" class="nav-link"><i
+                    class="fas fa-building"></i><span>Company</span></a>
+        </li>
 
-      <li class="dropdown">
-        <a href="" class="nav-link"><i class="fas fa-spinner"></i><span>Category</span></a>
-      </li>
+        <li class="dropdown">
+            <a href="" class="nav-link"><i class="fas fa-spinner"></i><span>Category</span></a>
+        </li>
 
-      <li class="dropdown">
-        <a href="index.html" class="nav-link"><i class="fas fa-poo"></i><span>Article</span></a>
-      </li>
+        <li class="dropdown">
+            <a href="index.html" class="nav-link"><i class="fas fa-poo"></i><span>Article</span></a>
+        </li>
 
-      <li class="dropdown">
-        <a href="index.html" class="nav-link"><i class="fab fa-adversal"></i><span>Advertise</span></a>
-      </li>
-      {{-- <li class="dropdown">
+        <li class="dropdown">
+            <a href="index.html" class="nav-link"><i class="fab fa-adversal"></i><span>Advertise</span></a>
+        </li>
+        {{-- <li class="dropdown">
         <a href="#" class="menu-toggle nav-link has-dropdown"><i
             data-feather="briefcase"></i><span>Widgets</span></a>
         <ul class="dropdown-menu">
@@ -37,4 +38,4 @@
         </ul>
       </li> --}}
     </ul>
-  </aside>
+</aside>
